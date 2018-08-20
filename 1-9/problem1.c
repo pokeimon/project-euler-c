90e6473b3e4da58812bc965f2d25d355c43979c2
Imon Daneshmand <pokeimon@users.noreply.github.com>
2018-08-20 15:32:27 -0700

    Project 2 and minor touchups

--- 1-9/problem1.c
+++ 1-9/problem1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
+/*
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 60851475143 ?
+ */
+int  main() {
 
-int main() {
-
-	int out = 0;
+	int sum = 0;
 	int i = 0;
 
 	for (i; i < 1000; i++) {
 		if (i % 3 == 0 || i % 5 == 0) {
-			out += i;
+			sum += i;
 		}
 	}
-	printf("Value is: %d", out);
+	printf("Value is: %d", sum);
+	
+	return 0;
 }
